b929fbb94a7d16f65d9f36d02af8b7be80a0e6a4
Timothy Antonius <tim.antonius@outlook.com>
2019-02-14 18:49:31 +0100

    user control

---
+++ ChaliceECMOSimulator/SingleParameterController.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ChaliceECMOSimulator
+{
+    public sealed partial class SingleParameterController : UserControl
+    {
+
+        public SingleParametersViewModel ViewModel { get; set; }
+
+        public SingleParameterController()
+        {
+            this.InitializeComponent();
+            this.ViewModel = new SingleParametersViewModel();
+        }       
+
+        private void ButStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
